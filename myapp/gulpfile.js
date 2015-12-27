4b9334687175848592fc2d3578af835bbdb52c7f
Deuvarney <deuvarney@yahoo.com>
2015-12-27 13:46:45 -0500

    Configured gulpfile. Added Lorem ipsum content to index.jade

--- myapp/gulpfile.js
+++ myapp/gulpfile.js
@@ -3,16 +3,18 @@ var jshint = require('gulp-jshint');
 var jshint_stylish = require('jshint-stylish');
 var uglifyjs = require('gulp-uglifyjs');
 var copy = require('gulp-copy');
+var sass = require('gulp-sass');
+
 var dest = 'build';
 
 // JS hint task
 gulp.task('process-js', function() {
 	//ONly looking at one file
 	//Looks like concatenation
-  gulp.src(['src/routes/*.js', 'src/**/*.js', 'src/public/js/*.js'])
+  gulp.src([ 'src/public/js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter(jshint_stylish))
-    .pipe(uglifyjs())
+    //.pipe(uglifyjs())
     .pipe(gulp.dest(dest));
 });
 
@@ -21,17 +23,27 @@ gulp.task('copy-unchanged-content', function(){
 	gulp.src([
 			'src/public/images/*', //Public Images
 			'src/bin/*', //Runtime files
-			'src/views/*' //Jade files
+			'src/views/*', //Jade files
+			'src/routes/*', //Routes
+			'src/app.js' // Express App.js file
 		])
 	.pipe(copy(dest, {prefix : 1}))
 });
 
-gulp.task('build',['process-js', 'copy-unchanged-content'], function(){})
+
+ 
+gulp.task('sass', function () {
+  gulp.src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task('build',['process-js', 'copy-unchanged-content', 'sass'], function(){})
 
 gulp.task('watch', function(){
 	gulp.watch('src/**/*', ['build']);
 });
 
-gulp.task('default',['watch'], function() {
+gulp.task('default',['build','watch'], function() {
   // place code for your default task here
 });
